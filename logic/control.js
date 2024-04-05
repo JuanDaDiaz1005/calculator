284ab0fc020438b57ea7e3ef7ed7551d11b6d28a
JuanDaDiaz1005 <juan.1701920068@ucaldas.edu.co>
2024-04-04 22:36:37 -0500

    Initial styles, clear button is functional

--- logic/control.js
+++ logic/control.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    clearDisplay();
+    buttons();
+    
 });
 
+function buttons(){
+    const clearButton = document.querySelector('.clear');
+    clearButton.addEventListener('click', clearDisplay);
+
+    const btn_nine = document.querySelector('.nine');
+    btn_nine.addEventListener('click', function() {
+        const display = document.querySelector('.display');
+        display.innerHTML += '9';
+    });
+}
+
 function clearDisplay() {
-    const display = document.getElementById('display');
+    const display = document.querySelector('.display');
     display.innerHTML = '';
 }
